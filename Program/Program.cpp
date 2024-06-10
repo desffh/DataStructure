2eff4d2ca5532d28232b552db635048de9783212
desffh <yangju058@gmail.com>
2024-06-10 19:43:32 +0900

    (2024.6.10) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+
+template <typename T>
+class SingleLinkedList
+{
+private:
+	int size;
+
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+
+	Node* head;
+
+public:
+	SingleLinkedList()
+	{
+		size = 0;
+		head = nullptr;
+
+	}
+	void PushFront(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node; //동적할당
+			head->data = data; //매개변수 넣어주기
+			head->next = nullptr;
+		}
+		else
+		{
+			Node* newNode = new Node;
+			newNode->data = data;
+			newNode->next = head;
+
+			head = newNode;
+		}
+
+		size++;
+	}
+	void Show()
+	{
+		Node* currentNode = head;
+
+
+		while (currentNode != nullptr)
+		{
+			cout << currentNode->data << endl;
+			currentNode = currentNode->next;
+		}
+	}
+
+
+};
+
+
+int main()
+{
+	SingleLinkedList<int> singlelinkedlist;
+}
