3e3bca0535ee25424629a33e997cc072b9b62308
desffh <yangju058@gmail.com>
2024-06-11 19:45:22 +0900

    (2024.6.11) 양방향 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -23,6 +23,24 @@ public:
 		head = nullptr;
 
 	}
+	void PopFront()
+	{
+		if (head != nullptr)
+		{
+			Node * deleteNode = head;
+			head = deleteNode->next;
+			delete deleteNode;
+			size--;
+
+		}
+		else
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+		
+
+	}
+
 	void PushFront(T data)
 	{
 		if (head == nullptr)
@@ -42,6 +60,70 @@ public:
 
 		size++;
 	}
+
+	void PushBack(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node;
+			head->data = data;
+			head->next = nullptr;
+		}
+		else
+		{
+			Node * currentNode = head;
+			
+			
+
+			while (currentNode->next != nullptr)
+			{
+				currentNode = currentNode->next;
+			}
+			Node * newNode = new Node;
+			
+			currentNode->next = newNode;
+		
+			newNode->data = data;
+			newNode->next = nullptr;
+		}
+
+		size++;
+	}
+
+	void PopBack()
+	{
+		if (head == nullptr)
+		{
+			cout << "Linked List is Empty" << endl;
+		}
+
+		else
+		{
+			Node * deleteNode = head;
+			Node * previousNode = nullptr;
+			
+			if (size == 1)
+			{
+				head = deleteNode->next;
+
+				delete deleteNode;
+			}
+			else
+			{
+				while (deleteNode->next != nullptr)
+				{
+					previousNode = deleteNode;
+					deleteNode = deleteNode->next;
+				}
+
+			}
+			previousNode->next = deleteNode->next;
+
+			delete deleteNode;	
+		}
+		size--;
+	}
+
 	void Show()
 	{
 		Node* currentNode = head;
@@ -61,4 +143,23 @@ public:
 int main()
 {
 	SingleLinkedList<int> singlelinkedlist;
+
+	//singlelinkedlist.PushFront(30);
+	//singlelinkedlist.PushFront(20);
+	//singlelinkedlist.PushFront(10);
+
+	//singlelinkedlist.PopFront();
+	//singlelinkedlist.PopFront();
+
+	singlelinkedlist.PushBack(10);
+	singlelinkedlist.PushBack(20);
+	singlelinkedlist.PushBack(30);
+
+	singlelinkedlist.PopBack();
+
+
+	singlelinkedlist.Show();
+
+	return 0;
+
 }
