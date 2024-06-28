8830a34fca632b6fbc2fa0c15f13bcf1a8e4edd7
desffh <yangju058@gmail.com>
2024-06-28 19:47:13 +0900

    (2024.6.28) 균형 이진 트리

---
+++ Class11th (Heap)/Source.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#define SIZE 8
+using namespace std;
+
+template <typename T>
+class Heap
+{
+private:
+
+    int index;
+
+    T Container[SIZE];
+
+public:
+    Heap()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            Container[i] = NULL;
+        }
+
+        index = 0;
+    }
+    void Insert(T data)
+    {
+        if (index >= SIZE)
+        {
+            cout << "Heap Overflow" << endl;
+        }
+        else
+        {
+            Container[++index] = data;
+
+            int child = index;
+            int parent = child / 2;
+
+            while (child > 1)
+            {
+                if (Container[parent] < Container[child])
+                {
+                    std::swap(Container[parent], Container[child]);
+                }
+
+                child = parent;
+
+                parent = child / 2;
+            }
+        }
+    }
+
+    T Remove()
+    {
+        if (index <= 0)
+        {
+            cout << "Heap is Empty" << endl;
+
+            exit(1);
+        }
+        else
+        {
+            //임시변수에 1번째 데이터 저장
+            T result = Container[1];
+
+            Container[1] = Container[index];
+
+            Container[index] = NULL;
+
+            index--;
+
+
+            int parent = 1;
+
+            while (parent * 2 <= index)
+            {
+                int child = parent * 2;
+
+                if (Container[child] > Container[child + 1])
+                {
+                    child++;
+                }
+
+                if (Container[child] < Container[parent])
+                {
+                    break;
+                }
+                else
+                {
+                    std::swap(Container[parent], Container[child]);
+                    parent = child;
+                }
+            }
+            return result;
+        }
+    }
+
+    void Show()
+    {
+        for (int i = 1; i <= index; i++) //index 0 사용안하기 때문
+        {
+            cout << Container[i] << " ";
+        }
+    }
+};
+
+
+int main()
+{
+    Heap<int> heap;
+
+    heap.Insert(6);
+    heap.Insert(7);
+    heap.Insert(2);
+    heap.Insert(10);
+
+    cout << "Heap의 최대값: " << heap.Remove() << endl;
+
+    heap.Show();
+
+    return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,108 +1,27 @@
 ﻿#include <iostream>
-#define SIZE 8
+
 using namespace std;
 
-template <typename T>
-class Heap
-{
-private:
 
-    int index;
-
-    T Container[SIZE];
-
-public:
-    Heap()
-    {
-        for (int i = 0; i < SIZE; i++)
-        {
-            Container[i] = NULL;
-        }
-
-        index = 0;
-    }
-    void Insert(T data)
-    {
-        if (index >= SIZE)
-        {
-            cout << "Heap Overflow" << endl;
-        }
-        else
-        {
-            Container[++index] = data;
-
-            int child = index;
-            int parent = child / 2;
-
-            while (child > 1)
-            {
-                if (Container[parent] < Container[child])
-                {
-                    std::swap(Container[parent], Container[child]);
-                }
-
-                child = parent;
-
-                parent = child / 2;
-            }
-        }
-    }
-
-    T Remove()
-    {
-        if (index <= 0)
-        {
-            cout << "Heap is Empty" << endl;
-            
-            exit(1);
-        }
-        else
-        {
-            //임시변수에 1번째 데이터 저장
-            T result = Container[1];
-
-            Container[1] = Container[index];
-
-            Container[index] = NULL;
-
-            index--;
-
-
-            int parent = 1;
-            int leftchild = parent * 2;
-            int rightchild = parent * 2 + 1;
-
-            if (Container[leftchild] > Container[rightchild])
-            {
-                std::swap(Container[parent], Container[leftchild]);
-            }
-            if (Container[leftchild] < Container[rightchild])
-            {
-                std::swap(Container[parent], Container[rightchild]);
-            }
-        }
-    }
-
-    void Show()
-    {
-        for (int i = 1; i <= index; i++) //index 0 사용안하기 때문
-        {
-            cout << Container[i] << " ";
-        }
-    }
-};
 
 
 int main()
 {
-    Heap<int> heap;
+#pragma region 인접 리스트
+    // 그래프의 각 정점에 인접한 정점들을 연결 리스트로 
+    // 표현하는 방법입니다.
 
-    heap.Insert(6);
-    heap.Insert(7);
-    heap.Insert(2);
-    heap.Insert(10);
+    // 장점
+    // 그래프의 모든 간선 의 수를 0(V + E)로 표현할 수 있습니다.
 
-    heap.Show();
+    // 단점
+    // 두 정점을 연결하는 간선을 조회하거나 정점의 차수를 알기 위해서
+    // 정점의 인접 리스트를 모두 탐색해야 하므로,
+    // 정점의 차수만큼의 시간이 필요합니다.
+
+#pragma endregion
+
+
 
     return 0;
 }
