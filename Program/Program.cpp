d45fb07af5451f40a1acb785f3b7322d3628e50f
desffh <yangju058@gmail.com>
2024-07-02 19:46:26 +0900

    (2024.7.2) 레드 블랙 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -12,6 +12,7 @@ private:
         Node* left;
         Node* right;
     };
+    
     Node * root;
 
 public:
@@ -19,13 +20,143 @@ public:
     {
         root = nullptr;
     }
+
+    Node * Root()
+    {
+        return root;
+    }
+
+    // 노드생성
+    Node* CreateNode(T data)
+    {
+        Node* newNode = new Node;
+        newNode->data = data; 
+        newNode->left = nullptr;
+        newNode->right = nullptr;
+
+        return newNode;
+    }
+    
+    void Insert(T data)
+    {
+        if (root == nullptr) //노드가 하나도없으면
+        {
+            root = CreateNode(data);
+        }
+        else //노드가 여러개 있다면
+        {
+            Node * currentNode = root;
+
+            while (currentNode != nullptr)
+            {
+                if (currentNode->data < data)
+                {   
+                    if (currentNode->right == nullptr)
+                    {
+                        currentNode->right = CreateNode(data);
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode->left;
+                    }
+
+                }
+                else if(currentNode->data > data)
+                {
+                    if (currentNode->left == nullptr)
+                    {
+                        currentNode->left = CreateNode(data);
+                        break;
+                    }
+                    else
+                    {
+                        currentNode = currentNode->left;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    bool Find(T data)
+    {
+        Node * currentNode = root;
+
+        if (currentNode == nullptr)
+        {
+            return false;
+        }
+        else
+        {
+            if (currentNode->data == data)
+            {
+                return true;
+            }
+            else
+            {
+                if (currentNode->data > data)
+                {
+                    currentNode = currentNode->left;
+                }
+                else
+                {
+                    currentNode = currentNode->right;
+                }
+            }
+        }
+        return false;
+    }
+
+    void Destroy(Node * root)
+    {
+        if (root != nullptr)
+        {
+            Destroy(root->left);
+            Destroy(root->right);
+            delete root;
+        }
+    }
+
+
+    // 중위순회 출력
+    void Inorder(Node* root)
+    {
+        if (root != nullptr)
+        {
+            Inorder(root->left);
+            cout << root->data << " ";
+            Inorder(root->right);
+        }
+    }
+
+    ~BinarySearchTree()
+    {
+        Destroy(root);
+    }
+
 };
+
+
 
 
 int main()
 {
+    BinarySearchTree<int> binarysearchtree;
+
+    binarysearchtree.Insert(10);
+    binarysearchtree.Insert(7);
+    binarysearchtree.Insert(15);
+    binarysearchtree.Insert(5);
 
 
+    binarysearchtree.Inorder(binarysearchtree.Root());
+
+
+    cout << binarysearchtree.Find(7) << endl;
 
     return 0;
 }
